92ec2277cd5ce592d0f6e9f04c13cda8fd65e375
Arihara-Sayaka <1803.arinko@gmail.com>
2020-04-06 11:45:25 +0900

    記事一覧確認完了

--- index.php
+++ index.php
@@ -4,5 +4,47 @@ require_once('config.php');
 require_once('functions.php');
 
 $dbh = connectDb();
+//sql文の実行 
+$sql = 'SELECT * FROM trimmings';
+//プリペアドステートメント
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+//記事デーテの格納
+$trimmings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>記事投稿フォーム</title>
+</head>
+
+<body>
+
+  <h1>記事一覧ページ</h1>
+
+  <?php if (count($trimmings)) : ?>
+    <ul class="trimmings-list">
+      <?php foreach ($trimmings as $trimming) : ?>
+        <li>
+          <?php echo h($trimming['title']); ?><br>
+          <img src="../dog_picture/<?php echo h($trimming['picture']); ?>" alt="犬の写真"><br>
+          <?php echo h($trimming['body']); ?><br>
+          投稿日時: <?php echo h($trimming['created_at']); ?>
+          <hr>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php else : ?>
+    <p>投稿された記事はありません</p>
+  <?php endif; ?>
+</body>
+
+</html>
